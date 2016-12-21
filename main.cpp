c14ed28fd38a6e8fd3f87101eb62d8bc52a7751e
Nick Mah <nickjmah@gmail.com>
2016-12-21 15:29:23 -0800

    Added ADC Initialization and Enabled select pins

--- main.cpp
+++ main.cpp
@@ -47,11 +47,54 @@
  * RX[UART] = PE0 UART RX pin
  *
  */
+void Init_ADC3(void){
+
+	ADCSRA = 0x87 ;	//Turn On ADC and set prescaler (CLK/128)
+	ADCSRB = 0x00;	//turn off autotrigger
+	ADMUX = 0x43;    	//Set ADC channel ADC5, set compare voltage to AVcc
+}
+
+void Init_ADC4(void){
+
+	ADCSRA = 0x87;	//Turn On ADC and set prescaler (CLK/128)
+	ADCSRB = 0x00;	//turn off autotrigger
+	ADMUX = 0x44;    	//Set ADC channel ADC5, set compare voltage to AVcc
+}
+
+void Init_ADC5(void){
+
+	ADCSRA = 0x87;	//Turn On ADC and set prescaler (CLK/128)
+	ADCSRB = 0x00;	//turn off autotrigger
+	ADMUX = 0x45;    	//Set ADC channel ADC5, set compare voltage to AVcc
+}
+
+void Init_ADC6(void){
+
+	ADCSRA = 0x87;	//Turn On ADC and set prescaler (CLK/128)
+	ADCSRB = 0x00;	//turn off autotrigger
+	ADMUX = 0x46;    	//Set ADC channel ADC5, set compare voltage to AVcc
+}
+
+void Init_ADC7(void){
+
+	ADCSRA = 0x87;	//Turn On ADC and set prescaler (CLK/128)
+	ADCSRB = 0x00;	//turn off autotrigger
+	ADMUX = 0x47;   	//Set ADC channel ADC5, set compare voltage to AVcc
+}
+
 
 int main(){
-	uint8_t x = ADCSRA;
-	/** this is an exmple comment
-	 * this is just here to how
-	 * pushing works in git
-	 */
+
+	// Start Initialize ADCs
+	Init_ADC3();
+	Init_ADC4();
+	Init_ADC5();
+	Init_ADC6();
+	Init_ADC7();
+	// End Initialize ADCs
+
+	//Start Set pin Inputs and Outputs
+	DDRC |= (1 << S0) | (1 << S1) | (1 << S2) | (1 << S3);
+	//End Set Pin Inputs and Outputs
+
 }
