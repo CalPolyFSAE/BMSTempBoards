0f149e5e3122dd9d508e7116f0e80308500cc0af
Daniel Baron <tahoedbaron@gmail.com>
2016-12-21 01:53:48 -0800

    Changed settings to correct references and paths modified int to uint8_t and adcsra to ADCSRA

--- main.cpp
+++ main.cpp
@@ -1,2 +1,15 @@
-//Code for BMSTempBoards
+/*
+ * main.cpp
+ *
+ *  Created on: Dec 18, 2016
+ *      Author: nmah
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 #include <avr/io.h>
+
+int main(){
+	uint8_t x = ADCSRA;
+}
