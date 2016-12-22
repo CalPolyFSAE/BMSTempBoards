35b674d8427bdeb983e0f4b3a5b18443db15abd5
Jason Zhou <jzhou15@calpoly.edu>
2016-12-21 17:34:40 -0800

    Added set outputs, deleted avr_sck, changed initializing adcs function, but it's still wrong

--- main.cpp
+++ main.cpp
@@ -11,8 +11,6 @@
 #define OUT2		4
 #define OUT1		3
 
-#define AVR_SCK		1
-
 #define S0			3
 #define S1			2
 #define S2			1
@@ -31,12 +29,13 @@
 
 /** pin map on MCU:
  *
+ *Not sure what to do with XTAL1, XTAL2
+ *
  * OUT4 = PF7 Ain for  71-64, 7-0
  * OUT3 = PF6 Ain for 63-56, 15-8
  * OUT0 = PF5 Ain for 55-48, 23-16
  * OUT2 = PF4 Ain for 43-36, 35-28
  * OUT1 = PF3 Ain for 47-44, 27-24
- * AVR_SCK = PB1 SPI Clock
  * S0 = PC3 LSB selector
  * S1 = PC2
  * S2 = PC1
@@ -47,54 +46,29 @@
  * RX[UART] = PE0 UART RX pin
  *
  */
-void Init_ADC3(void){
+
+void Init_ADCs(void){
 
 	ADCSRA = 0x87 ;	//Turn On ADC and set prescaler (CLK/128)
 	ADCSRB = 0x00;	//turn off autotrigger
-	ADMUX = 0x43;    	//Set ADC channel ADC5, set compare voltage to AVcc
+	ADMUX = 0x43;    	//Set ADC channel ADC3, set compare voltage to AVcc
 }
-
-void Init_ADC4(void){
-
-	ADCSRA = 0x87;	//Turn On ADC and set prescaler (CLK/128)
-	ADCSRB = 0x00;	//turn off autotrigger
-	ADMUX = 0x44;    	//Set ADC channel ADC5, set compare voltage to AVcc
-}
-
-void Init_ADC5(void){
-
-	ADCSRA = 0x87;	//Turn On ADC and set prescaler (CLK/128)
-	ADCSRB = 0x00;	//turn off autotrigger
-	ADMUX = 0x45;    	//Set ADC channel ADC5, set compare voltage to AVcc
-}
-
-void Init_ADC6(void){
-
-	ADCSRA = 0x87;	//Turn On ADC and set prescaler (CLK/128)
-	ADCSRB = 0x00;	//turn off autotrigger
-	ADMUX = 0x46;    	//Set ADC channel ADC5, set compare voltage to AVcc
-}
-
-void Init_ADC7(void){
-
-	ADCSRA = 0x87;	//Turn On ADC and set prescaler (CLK/128)
-	ADCSRB = 0x00;	//turn off autotrigger
-	ADMUX = 0x47;   	//Set ADC channel ADC5, set compare voltage to AVcc
-}
-
-
+//still need to fix the adcs part
 int main(){
 
+	unsigned char i;
+
 	// Start Initialize ADCs
-	Init_ADC3();
-	Init_ADC4();
-	Init_ADC5();
-	Init_ADC6();
-	Init_ADC7();
+	Init_ADCs();
 	// End Initialize ADCs
 
-	//Start Set pin Inputs and Outputs
-	DDRC |= (1 << S0) | (1 << S1) | (1 << S2) | (1 << S3);
-	//End Set Pin Inputs and Outputs
+	//Start Set outputs high
+	PORTC = (1 << PC3) | (1 << PC2) | (1 << PC1) | (1 << PC0);
+	//Define directions of port pins
+	DDRC = (1 << S0) | (1 << S1) | (1 << S2) | (1 << S3);
+	//Insert nop for synchronization
+	_NOP();
+	//Read port pins
+	i = PINC;
 
 }
